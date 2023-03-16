6378ea290469a1b70c87d3ae788e5dedc01f4a91
Otabek Rakhimov <rahimovmail@gmail.com>
2023-03-16 12:34:07 -0700

    Create functions to the Fund page

--- src/app/pages/Funds/Fund.tsx
+++ src/app/pages/Funds/Fund.tsx
@@ -16,5 +16,73 @@ import { Actions } from "../../../store";
 import { roundNumber, formatPercentage, formatFunds } from "../../../utils";
 
 export const Fund = () => {
-  <h2>Fund</h2>;
+  const {
+    state: {
+      funds: { data },
+    },
+    dispatch,
+  } = useStore();
+  const { fundId } = useParams();
+
+  const onSort = (sortDirection: "ASC" | "DESC", columnIdx: string) => {
+    dispatch({
+      type: Actions.SORT_FUNDS_INVESTEDIN,
+      payload: {
+        fundId: fundId!,
+        columnIdx: columnIdx,
+        sortDirection: sortDirection,
+      },
+    });
+  };
+
+  return (
+    <Table>
+      <TableHead onSort={onSort} id={fundId}>
+        <TableHeadCell>Company</TableHeadCell>
+        <TableHeadCell>Invested Round</TableHeadCell>
+        <TableHeadCell columnIdx="investedAmount" sortable>
+          Invested Amount
+        </TableHeadCell>
+        <TableHeadCell columnIdx="ownershipPercentage" sortable>
+          Ownership
+        </TableHeadCell>
+        <TableHeadCell columnIdx="impliedValue" sortable>
+          Implied Value
+        </TableHeadCell>
+        <TableHeadCell columnIdx="multiple" sortable>
+          Multiple
+        </TableHeadCell>
+      </TableHead>
+      <TableBody>
+        {fundId &&
+          data &&
+          data[fundId].investedIn.map((company) => {
+            return (
+              <TableRow key={company.id}>
+                <TableRowHeadCell>{company.name}</TableRowHeadCell>
+                <TableBodyCell>
+                  <Badge variant="green" light>
+                    {company.investedRound}
+                  </Badge>
+                </TableBodyCell>
+                <TableBodyCell>
+                  {formatFunds(company.investedAmount)}
+                </TableBodyCell>
+                <TableBodyCell>
+                  <Badge>{formatPercentage(company.ownershipPercentage)}</Badge>
+                </TableBodyCell>
+                <TableBodyCell>
+                  {formatFunds(company.impliedValue)}
+                </TableBodyCell>
+                <TableBodyCell>
+                  <Badge variant="blue" light>
+                    {roundNumber(company.multiple)}
+                  </Badge>
+                </TableBodyCell>
+              </TableRow>
+            );
+          })}
+      </TableBody>
+    </Table>
+  );
 };
