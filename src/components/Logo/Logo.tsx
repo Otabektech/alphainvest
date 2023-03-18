efe3fc29f5560b5900083aa7b49fcecdb2424137
Otabek Rakhimov <rahimovmail@gmail.com>
2023-03-18 14:19:35 -0700

    Add logo stylings

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const LogoBase = styled.span`
-    font-size: 2.625rem;
-    color: var(--generalColor-100);
-    font-weight: var(--font-weight-600);
+  font-size: 2.625rem;
+  color: var(--generalColor-100);
+  font-weight: var(--font-weight-600);
 `;
 export const Logo = () => {
-    return <LogoBase>investable.</LogoBase>;
+  return <LogoBase>investable</LogoBase>;
 };
