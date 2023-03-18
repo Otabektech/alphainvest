c1a78d4fa60783d992586318240ac19949a70c69
Otabek Rakhimov <rahimovmail@gmail.com>
2023-03-18 14:17:14 -0700

    Create Button filter functions

--- src/components/ButtonFilter/ButtonFilter.tsx
+++ src/components/ButtonFilter/ButtonFilter.tsx
@@ -3,35 +3,46 @@ import styled from "styled-components";
 import { BadgeBase } from "../Badge";
 
 const ClickableBadge = styled(BadgeBase)`
-    cursor: pointer;
+  cursor: pointer;
 `;
 
 const ButtonFilterBase = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 0 !important;
-    width: max-content;
-    height: 2.8rem;
-    padding: 0 var(--spacing-1);
-    background-color: var(--primaryColor-40);
-    border-radius: 0.4rem;
+  display: flex;
+  align-items: center;
+  gap: 0 !important;
+  width: max-content;
+  height: 2.8rem;
+  padding: 0 var(--spacing-1);
+  background-color: var(--primaryColor-40);
+  border-radius: 0.4rem;
 `;
 interface ButtonFilterProps {
-    activeIndex: string;
-    data: { label: string; value: string }[];
-    onClick: (value: string) => void;
+  activeIndex: string;
+  data: { label: string; value: string }[];
+  onClick: (value: string) => void;
 }
 
 const ButtonFilter: FC<ButtonFilterProps> = ({
-    activeIndex,
-    data,
-    onClick
+  activeIndex,
+  data,
+  onClick,
 }) => {
-    return (
-        <ButtonFilterBase>
-           <h2>Button filter</h2>
-        </ButtonFilterBase>
-    );
+  return (
+    <ButtonFilterBase>
+      {data.map((item) => {
+        return (
+          <ClickableBadge
+            variant="blue"
+            light={activeIndex !== item.value}
+            onClick={() => onClick(item.value)}
+            key={item.value}
+          >
+            {item.label}
+          </ClickableBadge>
+        );
+      })}
+    </ButtonFilterBase>
+  );
 };
 
 export { ButtonFilter };
