1bafc5646f5052049a56a780288a5e25c25bd139
Otabek Rakhimov <rahimovmail@gmail.com>
2023-03-18 14:20:52 -0700

    Add Table component styling

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Table = styled.div`
-    height: 100%;
-    overflow-y: auto;
-    overflow-x: auto;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: auto;
 `;
 
-export const Seperator = styled.div`
-    width: 0.1rem;
-    background-color: var(--generalColor-40);
-    height: 100%;
+export const Separator = styled.div`
+  width: 0.1rem;
+  background-color: var(--generalColor-40);
+  height: 100%;
 `;
