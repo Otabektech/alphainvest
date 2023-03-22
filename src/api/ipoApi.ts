ea1c6c9816e86bcfccf78f8a4418e9add58b4e3b
Otabek Rakhimov <rahimovmail@gmail.com>
2023-03-22 10:14:18 -0700

    Complete the project

--- src/api/ipoApi.ts
+++ src/api/ipoApi.ts
@@ -1,16 +1,17 @@
 class FinnhubAPI {
-    async fetchIPOCalendar(from: string, to: string) {
-        try {
-            const response = await fetch(
-                `https://finnhub.io/api/v1/calendar/ipo?from=${from}&to=${to}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`
-            );
-            const data = await response.json();
+  async fetchIPOCalendar(from: string, to: string) {
+    try {
+      const response = await fetch(
+        `https://finnhub.io/api/v1/calendar/ipo?from=${from}&to=${to}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data, "------");
 
-            return data;
-        } catch (error) {
-            throw error;
-        }
+      return data;
+    } catch (error) {
+      throw error;
     }
+  }
 }
 
 export { FinnhubAPI };
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { AppRouter } from "./app/router";
 import * as serviceWorker from "./serviceWorker";
 import { AppProvider } from "./context/AppContext";
 
-const root = document.getElementById("root");
+const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <AppProvider>
       <AppRouter />
     </AppProvider>
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
